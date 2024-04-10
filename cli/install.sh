1cc7f65f9341a9bb06a95d10e289fd85179c6f8d
Andreas Drobisch <andreas.drobisch@commercetools.com>
2024-04-10 17:05:07 +0200

    fix(naming): fix old name references

--- cli/install.sh
+++ cli/install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cargo build --release && sudo cp target/release/tresor-cli /usr/local/bin/tresor
+cargo build --release && sudo cp target/release/tresor /usr/local/bin/tresor
 echo "you can use tresor now"
